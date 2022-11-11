88849aba913cec8898e35d3f506632437a58375e
Gomathigade <112018647+Gomathigade@users.noreply.github.com>
2022-11-11 00:44:59 -0600

    added func

--- controllers/Aeroplane.js
+++ controllers/Aeroplane.js
@@ -27,8 +27,16 @@ exports.aeroplane_view_all_Page = async function(req, res) {
 }; 
 
 // for a specific Aeroplane. 
-exports.aeroplane_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Aeroplane detail: ' + req.params.id); 
+exports.aeroplane_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Aeroplane.findById(req.params.id) 
+        console.log(result)
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle Aeroplane create on POST. 
@@ -55,6 +63,26 @@ exports.aeroplane_delete = function(req, res) {
 }; 
  
 // Handle Aeroplane update form on PUT. 
-exports.aeroplane_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Aeroplane update PUT' + req.params.id); 
+exports.aeroplane_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    let toUpdate = await Aeroplane.findById( req.params.id) 
+    try { 
+        // Do updates of properties 
+        if(req.body.flight_capacity)  
+            toUpdate.flight_capacity = req.body.flight_capacity; 
+        if(req.body.flight_number) 
+            toUpdate.flight_number = req.body.flight_number; 
+        if(req.body.flight_captain) 
+            toUpdate.flight_captain = req.body.flight_captain; 
+        if(req.body.is_operational) 
+            toUpdate.is_operational = true; 
+        else
+            toUpdate.is_operational = false;
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
 }; 
--- routes/Aeroplane.js
+++ routes/Aeroplane.js
@@ -5,4 +5,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', aeroplane_controlers.aeroplane_view_all_Page ); 
 
+/* GET request for one aeroplane. */
+router.get('/:id', aeroplane_controlers.aeroplane_detail ); 
+
+/* PUT request to update aeroplane. */
+router.put('/:id', aeroplane_controlers.aeroplane_update_put ); 
+
 module.exports = router;
