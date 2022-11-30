32d71236972a57f9d793f699da7e8d5e2106f22a
Gomathigade <112018647+Gomathigade@users.noreply.github.com>
2022-11-30 17:53:56 -0600

    added validation

--- models/Aeroplane.js
+++ models/Aeroplane.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose") 
 const AeroplaneSchema = mongoose.Schema({ 
-flight_capacity: Number, 
-flight_number: Number, 
-flight_captain: String, 
-is_operational: Boolean
+flight_capacity: { type: Number, required: true, min: 40 }, 
+flight_number: { type: Number, required: true, min: 1000 , max: 9999},
+flight_captain: { type: String, required: true }, 
+is_operational: { type: Boolean, required: true }
 }) 
  
 module.exports = mongoose.model("CostuAeroplaneme", 
--- routes/Aeroplane.js
+++ routes/Aeroplane.js
@@ -2,6 +2,17 @@ var express = require('express');
 const aeroplane_controlers= require('../controllers/Aeroplane'); 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET home page. */
 router.get('/', aeroplane_controlers.aeroplane_view_all_Page ); 
 
@@ -9,7 +20,7 @@ router.get('/', aeroplane_controlers.aeroplane_view_all_Page );
 router.get('/create', aeroplane_controlers.aeroplane_create_Page);
 
 // PUT/UPDATE request to update aeroplane.  
-router.get('/update', aeroplane_controlers.aeroplane_update_Page); 
+router.get('/update', secured, aeroplane_controlers.aeroplane_update_Page); 
 
 // GET request for one aeroplane. 
 router.get('/detail', aeroplane_controlers.aeroplane_view_one_Page); 
